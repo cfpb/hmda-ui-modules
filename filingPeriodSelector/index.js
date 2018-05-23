62ba2cc2684233ed47c595e330ab0f812ed964ac
Andrew <awolfe76@gmail.com>
2018-05-23 10:14:23 -0400

    fix React warning ... 'Warning: Use the  or  props on <select> instead of setting  on <option>.'

--- filingPeriodSelector/index.js
+++ filingPeriodSelector/index.js
@@ -3,22 +3,17 @@ import PropTypes from 'prop-types'
 
 const filingPeriods = ['2018', '2017']
 
-const isSelected = (loopPeriod, selectedPeriod) => {
-  if (loopPeriod === selectedPeriod) return true
-  return false
-}
-
 const FilingPeriodSelector = props => {
   return (
     <form className="FilingPeriodSelector usa-form">
-      <select onChange={props.onChange}>
+      <select
+        defaultValue={filingPeriods[0]}
+        value={props.filingPeriod}
+        onChange={props.onChange}
+      >
         {filingPeriods.map(filingPeriod => {
           return (
-            <option
-              key={filingPeriod}
-              value={filingPeriod}
-              selected={isSelected(filingPeriod, props.filingPeriod)}
-            >
+            <option key={filingPeriod} value={filingPeriod}>
               {filingPeriod}
             </option>
           )
